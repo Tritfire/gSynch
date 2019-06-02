f8b2994a0f1bb6fb96207c2e0a4d51b2a3d6d58e
Gabyfle <gaby.santamaria@outlook.fr>
2019-06-02 22:34:15 +0200

    First version
    - It's working but in beta cuz u know, I'm not paid to test my stuff

--- main.py
+++ main.py
@@ -8,14 +8,16 @@
 #      IMPORTS       #
 #--------------------#
 import json, requests
-import sys, os
+import sys, os, zipfile
 import urllib.request
 import datetime
 #--------------------#
 #   CONFIGURATION    #
 #--------------------#
-steam = '13C34ABF0073127B1904B632B7391356' # Your Steam API key
-github = 'e932228baa42d68c35368dfa7a24fecd8a0b4095' # Your Github API key
+steam = '' # Your Steam API key
+github = '' # Your Github API key
+gmad = '' # Your path to gmad.exe
+gmpublish = '' # Your path to gmpublish.exe
 
 #------------------------------#
 #                              #
@@ -40,12 +42,57 @@ def isZip(ftype):
             return True
     return False
 
+def unZip(path, fname):
+    if not os.path.isfile(path + '/' + fname):
+        raise Exception('A problem occurred while trying to unzip the file archive : file do not exists nor is a valid one.')
+    f = zipfile.ZipFile(path + '/' + fname, 'r')
+    f.extractall(path)
+    f.close()
+
 def createDirectory(dname):
     if not os.path.exists(dname):
         os.makedirs(dname)
 
 def createEmptyFile(fname, relativeDir):
     open(relativeDir + '/' + fname, 'w').close()
+
+# TOOK & ADAPTED FROM THIS POST : https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder-in-python
+def clearDirectory(folder):
+    for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+        try:
+            _, ext = os.path.splitext(path)
+            if os.path.isfile(path) and ext != '.json':
+                os.unlink(path)
+        except Exception as e:
+            print(e)
+
+def getBaseDirectory():
+    blacklist = {
+        'maps',
+        'backgrounds',
+        'gamemodes',
+        'materials',
+        'lua',
+        'scenes',
+        'models',
+        'scripts',
+        'particles',
+        'sound',
+        'resource'
+    }
+    i = 0
+    directories = [f.path for f in os.scandir('tmp') if f.is_dir()]
+    for p in directories:
+        if os.path.isdir(os.path.join('tmp', p)):
+            i = i + 1
+    if i > 1:
+        return 'tmp'
+    else:
+        if directories[0] in blacklist:
+            return 'tmp'
+        else:
+            return directories[0]
 
 #--------------------#
 #     STEAM PART     #
@@ -68,6 +115,18 @@ def getSteamLastUpdateTime(key, appid, fileid):
     time = data['response']['publishedfiledetails'][0]['time_updated']
     
     return time
+
+def createWorkshopArchive(path, basePath):
+    fullPath = os.path.dirname(os.path.realpath(__file__)) + '/' + basePath
+    status = os.system(gmad + ' create -folder ' + fullPath + ' -out ' + fullPath)
+    if status != 0:
+        raise Exception('The GMA proccess ended with an error.')
+
+def updateWorkshopItem(gmaPath, fileId, changes):
+    fullPath = os.path.dirname(os.path.realpath(__file__)) + '/' + gmaPath
+    status = os.system(gmpublish + ' update -addon "' + fullPath + '" -id "' + fileId + '" -changes "' + changes + '"')
+    if status != 0:
+        raise Exception('The GMPUBLISH proccess ended with an error.')
 
 #--------------------#
 #    GITHUB  PART    #
@@ -125,15 +184,41 @@ def Main():
     
     data = getGithubReleaseData(github, repoName)
 
+    # Checking /tmp/ directory + creating the empty file
     print('Preparing the download...')
     createDirectory('tmp')
     createEmptyFile(data['name'], 'tmp')
 
+    # Downloading and writing the file into the previously created empty file
     t = datetime.datetime.now().timestamp()
-    print('Starting to download the latest release of https://github.com/' + repoName + ' ... ...')
+    print('Starting to download the latest release of https://github.com/' + repoName + ' ... ' + str(data['size']) + ' ...')
     urllib.request.urlretrieve(data['downloadUrl'], 'tmp/' + data['name'])
     print('Download finished with success. It took : ' + str(datetime.datetime.now().timestamp() - t) + ' seconds')
 
+    # Unzipping the downloaded file
+    t = datetime.datetime.now().timestamp()
+    print('Starting to unzip : /tmp/' + data['name'] + '...')
+    unZip('tmp', data['name'])
+    print('Uncompression finished with success. It took : ' + str(datetime.datetime.now().timestamp() - t) + ' seconds')
 
+    # Clearing the useless files from the folder (like .zip, LICENCE, .gitattributes, README.md, etc...) [WE EXCLUDE .JSON FILES FROM DELETION]
+    print('Deleting useless files ...')
+    clearDirectory('tmp')
+
+    # Creating the .gma archive
+    t = datetime.datetime.now().timestamp()
+    print('Starting to create the .gma archive...')
+    baseName = getBaseDirectory()
+    createWorkshopArchive(gmad, baseName)
+    gmaName = baseName + '.gma'
+    print('The gma archive has been correctly created. It took : ' + str(datetime.datetime.now().timestamp() - t) + ' seconds')
+
+    # Updating the Workshop item
+    t = datetime.datetime.now().timestamp()
+    print('Starting to update the .gma archive...')
+    updateWorkshopItem(gmaName, fileid, data['body'])
+    print('The gma archive has been correctly uploaded to the Workshop. It took : ' + str(datetime.datetime.now().timestamp() - t) + ' seconds')
+
+    return 0
 
 Main()
