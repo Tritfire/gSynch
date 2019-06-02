311d95b288617efa2715b1a3a9ee415338580b8e
Gabyfle <gaby.santamaria@outlook.fr>
2019-06-02 15:34:02 +0200

    Update main.py
    - Finished times systems

--- main.py
+++ main.py
@@ -9,16 +9,23 @@
 #--------------------#
 import json
 import requests
+import datetime
+import sys
 #--------------------#
-#	CONFIGURATION	 #
+#   CONFIGURATION    #
 #--------------------#
 steam = '' # Your Steam API key
 github = '' # Your Github API key
 
+#------------------------------#
+#                              #
+#       HELPERS FUNCTIONS      #
+#                              #
+#------------------------------#
 #--------------------#
-#	  STEAM PART  	 #
+#     STEAM PART     #
 #--------------------#
-def getLastUpdateTime(key, appid, fileid):
+def getSteamLastUpdateTime(key, appid, fileid):
     baseUrl = 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/'
     parameters = {
         'key': key,
@@ -28,12 +35,65 @@ def getLastUpdateTime(key, appid, fileid):
     }
     r = requests.post(url = baseUrl, data = parameters)
     data = json.loads(r.text)
+    try:
+        data['response']['publishedfiledetails'][0]['time_updated']
+    except NameError:
+        raise Exception('Can\'t get the latest update time of the following WS item ' + fileid)
+
     time = data['response']['publishedfiledetails'][0]['time_updated']
-    if time is None:
-        time = 0
     
     return time
 
 #--------------------#
-#	 GITHUB  PART  	 #
+#    GITHUB  PART    #
 #--------------------#
+def getGithubLastUpdateTime(key, repoName):
+    baseUrl = 'https://api.github.com/repos/' + repoName + '/releases/latest?access_token=' + key
+    r = requests.get(baseUrl)
+    data = json.loads(r.text)
+
+    try:
+        iso = datetime.datetime.strptime(data['published_at'], "%Y-%m-%dT%H:%M:%S%z")
+    except NameError:
+        raise Exception('Can\'t fetch any attached asset to the latest release of ' + repoName)
+
+    return iso.timestamp()
+
+def getGithubReleaseText(key, repoName):
+    baseUrl = 'https://api.github.com/repos/' + repoName + '/releases/latest?access_token=' + key
+    r = requests.get(baseUrl)
+    data = json.loads(r.text)
+
+    try:
+        downloadUrl = data['assets'][0]['browser_download_url']
+    except NameError:
+        raise Exception('Can\'t fetch any attached asset to the latest release of ' + repoName)
+    try:
+        data['body']
+    except NameError:
+        raise Exception('An error occurred while trying to access to the latest release body of ' + repoName)
+
+    return {
+        'downloadUrl': downloadUrl,
+        'body': data['body']
+    }
+
+#--------------------#
+#        MISC        #
+#--------------------#
+def checkTimes(steamKey, appid, fileid, githubKey, repoName):
+    return getSteamLastUpdateTime(steamKey, appid, fileid) == getGithubLastUpdateTime(githubKey, repoName)
+
+#------------------------------#
+#                              #
+#         MAIN FUNCTION        #
+#                              #
+#------------------------------#
+def Main():
+    appid = str(sys.argv[1]) # Steam's APPID
+    fileid = str(sys.argv[2]) # Steam's Workshop FILEID
+    repoName = str(sys.argv[3]) # Github's repositery name FORMAT : <owner/name>
+
+    if checkTimes(steam, appid, fileid, github, repoName):
+        print('Everything is up to date. Your workshop file has already been synchronized with your Github repository.')
+        return True
