0a91d1bf3a39c4f132bc04a6fcaf036722d1646b
gabyfle <gaby.santamaria@outlook.fr>
2020-04-14 01:37:19 +0200

    steam.py has been improved
    - get_installation_path has been finished but it needs to be tested
    - the constructor now go and search for custom libraryfolders path and adds them to games_path
    
    - did a little research on how the fuck the libraryfolders.vdf file is working

--- gsynch/steam/steam.py
+++ gsynch/steam/steam.py
@@ -3,9 +3,19 @@
 # Date : 10/04/2020
 # License : Apache 2.0
 import os
+import sys
+# steamfiles library, made by @leovp https://github.com/leovp/steamfiles
+from steamfiles import acf
 
 if not os.name == "posix":
-    import winreg # assure portability
+    import winreg  # assure portability
+
+
+class NotFound(Exception):
+    """
+    This is raised when a required data isn't found by the system
+    """
+    pass
 
 
 class Steam:
@@ -28,8 +38,33 @@ class Steam:
         self.games_path = [
             self.steam_path + "\\steamapps"
         ]
-        # Check if there is other library directories
-        # TODO : parse libraryfolders.vdf with steamfiles lib (but we need to wait the maintener to accept the pull request)
+        # Check if there is other library directories (see "libraryfolders.vdf analysis.txt")
+        try:
+            with open(self.steam_path + "\\libraryfolders.vdf") as foldersfile:
+                data = acf.load(foldersfile)
+        except OSError as e:
+            print("We can't figure out where the fuck is your libraryfolders.vdf file !"
+                  f"Full error :\n {e}")
+            sys.exit(-1)  # this is a fatal error
+        except TypeError or ValueError as e:
+            print(
+                f"An error occurred while trying to parse {self.steam_path}\\libraryfolders.vdf. Please open an Issue "
+                f"at https://github.com/Gabyfle/gSynch with your libraryfolders.vdf file !")
+            sys.exit(-1)  # this is a fatal error
+        finally:
+            if foldersfile is not None:
+                foldersfile.close()
+        # Now check if the libraryfolders.vdf file is correct, and if he follow "libraryfolders.vdf analysis.txt" (if
+        # not, abort)
+        if not data["LibraryFolders"]:
+            print(f"An error occurred while trying to use data from your libraryfolders.vdf file. Please report this "
+                  f"to https://github.com/Gabyfle/gSynch in the Issue section, attaching your libraryfolders.vdf file.")
+            sys.exit(-1)  # this is a fatal error
+        else:
+            for key in data["LibraryFolders"]:
+                if key.isdigit():  # if the key is a digit, it's a path to a library folder
+                    self.games_path.append(data["LibraryFolders"][key])
+        # now we should have all the library path from the user's Steam installation
 
     def is_installed(self, app_id):
         """
@@ -51,13 +86,37 @@ class Steam:
     def get_install_path(self, app_id):
         """
         Get the installation path of the application "app_id"
-        :param app_id:
-        :return:
+        Args:
+            app_id: Game's steam id
+
+        Returns:
+            str: path to the game
         """
+        data = dict  # contains parsed data from appmanifest_id.acf files
+
         for path in self.games_path:
             for file in os.listdir(path):
-                if os.path.isfile((os.path.join(path, file))):
-                    if app_id in file:
-                        if os.path.splitext(file)[1] == ".acf":
-                            pass # parse .acf file to find the
-        return path
+                if os.path.isfile((os.path.join(path, file))):  # make sure this is a file and not a directory
+                    if app_id in file:  # this file contains the application id of the requested app
+                        if os.path.splitext(file)[1] == ".acf":  # make sure this is a manifest file
+                            try:
+                                with open(os.path.join(path, file)) as acf_file:
+                                    data = acf.load(acf_file)
+                            except OSError as e:
+                                print(f"A problem occurred while trying to open {os.path.join(path, file)}. Make sure "
+                                      f"gSynch has the permission to read this path!")
+                                sys.exit(-1)  # this is a fatal error
+                            except TypeError or ValueError as e:
+                                print(
+                                    f"An error occurred while trying to parse {os.path.join(path, file)}. Please open "
+                                    f"an Issue "
+                                    f"at https://github.com/Gabyfle/gSynch with {os.path.join(path, file)} attached!")
+                                sys.exit(-1)  # this is a fatal error
+                            finally:
+                                if acf_file is not None:
+                                    acf_file.close()
+        if data is not None and data["installDir"] is not None:
+            directory = data["installDir"]
+        else:
+            raise NotFound(f"Sorry, but gSynch can't find the app : {app_id}... Are you sure it's installed ?")
+        return directory
